749dcf0f613ad1a172bfc65e44446eb66db3646c
Oron Port <soronpo@campus.technion.ac.il>
2017-12-10 19:43:46 +0200

    Added BetterChecked workaround

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -6,3 +6,15 @@ object Test {
 
   myCheck(42)
 }
+
+
+//Workaround with inheritance
+object BetterTest {
+  type Cond[T] = T
+  final class MyCheckedType[T](value : Int) extends BetterChecked[MyCheckedType, Cond, T](value)
+  object MyCheckedType extends BetterCheckedCO[MyCheckedType, Cond]
+
+  def myCheck[T](value : MyCheckedType[T]) : Unit = {} //also fails without type aliasing (value : Checked[Cond, T])
+
+  myCheck(42)
+}
--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -14,8 +14,33 @@ object Builder {
       import c.universe._
       val condTpe = weakTypeOf[Cond[_]]
       val tTpe = weakTypeOf[T]
+      print(s"Builder's Cond looks like: $condTpe")
       if (condTpe.typeArgs.isEmpty) c.abort(c.enclosingPosition, "Cond should have type arguments. Bad Reflection!")
       q"new Checked[$condTpe, $tTpe](1)"
+    }
+  }
+}
+
+
+class BetterChecked[Chk[_], Cond[_], T](val value : Int)
+trait BetterCheckedCO[Chk[_], Cond[_]] { //to make companion object for BetterChecked
+  implicit def fromNumValue[T](value : T) : Chk[T] = macro BetterBuilder.Macro.fromNumValue[Chk, Cond, T]
+}
+
+object BetterBuilder {
+  final class Macro(val c: Context) {
+    def fromNumValue[Chk[_], Cond[_], T](value : c.Tree)(
+      implicit
+      chk : c.WeakTypeTag[Chk[_]], cond : c.WeakTypeTag[Cond[_]], t : c.WeakTypeTag[T]
+    ): c.Tree = {
+      import c.universe._
+      val chkSym = symbolOf[Chk[_]]
+      val chkTpe = weakTypeOf[Chk[_]]
+      val condTpe = weakTypeOf[Cond[_]]
+      val tTpe = weakTypeOf[T]
+      print(s"BetterBuilder's Cond looks like: $condTpe")
+      if (condTpe.typeArgs.isEmpty) c.abort(c.enclosingPosition, "Cond should have type arguments. Bad Reflection!")
+      q"new $chkSym[$tTpe](1)"
     }
   }
 }
