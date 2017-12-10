c7112072996e524a6dfcc01a44c282a314544ef6
Oron Port <soronpo@campus.technion.ac.il>
2017-12-10 17:09:46 +0200

    update message

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -2,7 +2,7 @@ object Test {
   type Cond[T] = T
   type MyCheckedType[T] = Checked[Cond, T]
 
-  def myCheck[T](value : MyCheckedType[T]) : Unit = {}
+  def myCheck[T](value : MyCheckedType[T]) : Unit = {} //also fails without type aliasing (value : Checked[Cond, T])
 
   myCheck(42)
 }
